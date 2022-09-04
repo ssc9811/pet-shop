00ac42ba96f96b79e5f952ea1163da4e0b415be6
ssc9811 <poer2418@naver.com>
2022-09-04 15:18:02 +0900

    Update Adoption.sol

--- Adoption.sol
+++ Adoption.sol
@@ -1,7 +1,9 @@
 pragma solidity ^0.4.17;
 contract Adoption {
+    // 이더리움 Address의 배열
     address[4] public adopters;
 
+    // 입양 요청
     function adopt(uint petId) public returns (uint) {
         require(petId >= 0 && petId <= 3);
 
@@ -10,8 +12,8 @@ contract Adoption {
         return petId;
     }
 
+    // 입양 상태
     function getAdopters() public view returns (address[4]) {
         return adopters;
     }
 }
-
